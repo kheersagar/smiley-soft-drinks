4958e55363556bb1713ae6309a0c74d4e932ed28
kheersagar <santparja@gmail.com>
2022-04-17 17:57:24 +0530

    added parallax effect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,10 @@ import "./Home.css";
 
 //image
 import landing from "../../Images/landing.svg"
+import tab1 from "../../Images/tab1.png"
+import waterwave from "../../Images/waterwave.png"
+import trees from "../../Images/trees.png"
+import sky from "../../Images/sky.png"
 import drinks from "../../Images/drinks.svg"
 import Products from '../Products/Products';
 import Reviews from '../Reviews/Reviews';
@@ -16,14 +20,20 @@ function Home() {
   return (
     <div className='home_main' >
       <NavigationBar />
-      <div className='home_landing'>
+    <div className='wrapper'>
+    <div className='home_landing'>
       <div className='title'>
       The Drink of Odisha
       </div>
-          <img src={landing} className='home_landing_img'/>
+          {/* <img src={landing} className='home_landing_img'/> */}
+          <img src={landing} className='home_landing_img landing'/>
+          <img src={sky} className='home_landing_img sky'/>
+          <img src={tab1} className='home_landing_img tab1'/>
+          <img src={waterwave} className='home_landing_img waterwave'/>
+          <img src={trees} className='home_landing_img trees'/>
       </div> 
        {/*  */}
-       <div className='landing_section'>
+       <div className='landing_section'> 
               <div className='welcome'>Welcome to the drink of Odisha</div>
               <div className='manu'>Smiley soda / soft drink manufacturer</div>
           <div className='section_card'>
@@ -41,6 +51,7 @@ function Home() {
        {/*  */}
        <Products />
        <Reviews />
+    </div>
     </div>
   )
 }
